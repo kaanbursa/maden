74c8edeeb1032490dadf376b0020f0761e36824e
kaan bursa <kaanbursa9@gmail.com>
2017-04-11 21:45:18 +0200

    commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x53, 0xA2, 0xA9, 0x11, 0x04, 0x4f, 0x9e, 0x4f, 0x35, 0x05, 0x00, 0xA2, 0x1A, 0xA1, 0xAA, 0x19 } };
+}
